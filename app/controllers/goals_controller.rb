ed7696530957b7d78c9936a3745e4135f16c8a93
EC2 Default User <ec2-user@ip-172-31-16-35.ap-northeast-1.compute.internal>
2021-02-08 08:23:29 +0000

    finish 2-6

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,18 +1,15 @@
 class GoalsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_goal, only: [:show, :edit, :update, :destroy]
 
   # GET /goals
   def index
-    @goals = Goal.all
-  end
-
-  # GET /goals/1
-  def show
+    @goals = current_user.goals.all
   end
 
   # GET /goals/new
   def new
-    @goal = Goal.new
+    @goal = current_user.goals.new
   end
 
   # GET /goals/1/edit
@@ -21,37 +18,37 @@ class GoalsController < ApplicationController
 
   # POST /goals
   def create
-    @goal = Goal.new(goal_params)
-
+    @goal = current_user.goals.new(goal_params)
     if @goal.save
-      redirect_to @goal, notice: 'Goal was successfully created.'
+     @status = true
     else
-      render :new
+     @status = false
     end
   end
 
   # PATCH/PUT /goals/1
   def update
     if @goal.update(goal_params)
-      redirect_to @goal, notice: 'Goal was successfully updated.'
+      @status = true
     else
-      render :edit
+      @status = false
     end
   end
 
   # DELETE /goals/1
   def destroy
     @goal.destroy
-    redirect_to goals_url, notice: 'Goal was successfully destroyed.'
+    
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_goal
-      @goal = Goal.find(params[:id])
+      @goal = current_user.goals.find_by(id: params[:id])
+   redirect_to(goals_url, alert: "ERROR!!") if @goal.blank?
     end
 
-    # Only allow a trusted parameter "white list" through.
+    
     def goal_params
       params.require(:goal).permit(:title, :user_id)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :todos
-  resources :goals
+ resources :goals do
+ resources :todos do
+     member do
+       get "sort"
+     end
+   end
+ end
+ 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
