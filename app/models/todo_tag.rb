27ed70197e456149a3394e3a6a923b0a2163a41f
EC2 Default User <ec2-user@ip-172-31-16-35.ap-northeast-1.compute.internal>
2021-02-15 09:00:17 +0000

    finish 2-14

---
+++ app/models/todo_tag.rb
@@ -0,0 +1,7 @@
+class TodoTag < ApplicationRecord
+  belongs_to :todo
+  belongs_to :tag
+  
+  validates :todo_id, :uniqueness => { :scope => :tag_id }
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :tags
  resources :goals do
   resources :todos do
    member do
